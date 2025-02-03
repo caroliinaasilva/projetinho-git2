0152850714993dd0f803d19375330e6ec449c250
carolinadesousa84@yahoo.com <carolinadesousa84@yahoo.com>
2025-02-03 17:08:16 +0000

    usuaruisok

--- exercicios/usuarios.js
+++ exercicios/usuarios.js
@@ -1,16 +1,8 @@
-//4 - Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa 60,00 reais por dia e 0,15 centavos por km rodado.
+let nota1 = 30
+let nota2 = 20
+let nota3 = 50
 
 
-let kmPercorridos = 800
-let dias = 2
-let km = 0.15
-let totalPorDias = dias * 60
-let totalkm = kmPercorridos * km
+let soma = nota1 + nota2 + nota3 / 5
 
-let final = totalPorDias + totalkm
-
-
-let valorApagar = final
-
-
-console.log (`voce vai pagar ${valorApagar}`)
+console.log (`total ${soma}`)
