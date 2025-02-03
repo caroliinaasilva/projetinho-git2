7f847fdd22df4da0f0f979c7efd5c879f6992998
carolinadesousa84@yahoo.com <carolinadesousa84@yahoo.com>
2025-02-03 16:59:38 +0000

    carrook

---
+++ exercicios/carroAlugado.js
@@ -0,0 +1,13 @@
+let kmPercorridos = 800
+let dias = 2
+let km = 0.15
+let totalPorDias = dias * 60
+let totalkm = kmPercorridos * km
+
+let final = totalPorDias + totalkm
+
+
+let valorApagar = final
+
+
+console.log (`voce vai pagar ${valorApagar}`)
