8f1899c4690e485f3a3ad4b9da8c8ee1913bb906
carolinadesousa84@yahoo.com <carolinadesousa84@yahoo.com>
2025-02-03 16:33:23 +0000

    viagemdone

---
+++ exercicios/viagem.js
@@ -0,0 +1,11 @@
+
+//2 - Calcule o tempo de uma viagem de carro. 
+//Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let distance= 1000
+let velocity = 90
+
+let hoursTravelling = distance/velocity
+
+console.log (` it will take  ${hoursTravelling} hours travelling`)
+
